99a894c3e5c11cb0818c43acc2f16aca74c80a1c
Aris <titilayotijani15@gmail.com>
2025-01-20 00:35:56 +0100

    fix illegal invocation of navbar links

--- src/components/Bar.tsx
+++ src/components/Bar.tsx
@@ -2,16 +2,16 @@ const Bar = () => {
   return (
     <>
       <div className="navbar-nav gap-4 ">
-        <a className="nav-link" aria-current="page" href="#" data-bs-toggle="tab">
+        <a className="nav-link" aria-current="page" href="#" >
           Home
         </a>
-        <a className="nav-link" href="#about" data-bs-toggle="tab">
+        <a className="nav-link" href="#about" >
           About
         </a>
-        <a className="nav-link" href="#projects" data-bs-toggle="tab">
+        <a className="nav-link" href="#projects">
           Projects
         </a>
-        <a className="nav-link" href="#contact" data-bs-toggle="tab">
+        <a className="nav-link" href="#contact" >
           Contact
         </a>
       </div>
