16b17412ca8e77e9c236cac6e9196fb47ddbd910
Aris <titilayotijani15@gmail.com>
2026-01-06 17:34:23 +0100

    update URLs

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -8,13 +8,40 @@ const Footer = () => {
       <div className="d-flex justify-content-center align-items-center py-3 bg-dark text-light m-0">
         <p className="mb-0">© 2025 Aris</p>
         <div className="d-flex gap-3 ms-2">
-          <img src={frontendMentor} alt="FrontendMentor favicon" width={18} />
-          <img src={github} alt="Github icon" width={18} />
-          <img src={mail} alt="email icon" width={18} />
+          <a
+            href="https://www.frontendmentor.io/profile/Aishat02"
+            target="blank"
+            className="btn d-flex align-items-center p-0"
+            aria-label="View Frontend Mentor Profile"
+          >
+            <img
+              src={frontendMentor}
+              alt="Frontend-Mentor favicon"
+              width={18}
+            />
+          </a>
+
+          <a
+            href="https://github.com/Aishat02"
+            target="blank"
+            className="btn d-flex align-items-center p-0"
+            aria-label="View GitHub Profile"
+          >
+            <img src={github} alt="GitHub logo" width={18} />
+          </a>
+
+          <a
+            href="mailto:titilayotijani15@gmail.com"
+            target="blank"
+            className="btn d-flex align-items-center p-0"
+            aria-label="email address"
+          >
+            <img src={mail} alt="email icon" width={18} />
+          </a>
         </div>
       </div>
     </>
   );
-};
+};  
 
 export default Footer;
